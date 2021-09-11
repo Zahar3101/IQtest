d5cb6e528a26749aab2a982dd7eb7ba62d722a8e
Zahar3101 <43762006+Zahar3101@users.noreply.github.com>
2021-09-11 22:52:56 +0500

    Add files via upload

--- calc.php
+++ calc.php
@@ -1,5 +1,19 @@
 <?php
-    $value = $_POST['value'];
-    $output = $value+100;
-    echo $output;
+    $datepicker1 = $_POST['datepicker1'];
+    $summvk = $_POST['summvk'];
+    $srokvklada = $_POST['srokvklada'];
+    $popvk = $_POST['popvk'];
+    $summpvk = $_POST['summpvk'];
+
+    $summn1 = (float) $summvk;
+    $summad = (float) $summpvk;
+
+    if ($popvk = false)
+        $summn = $summn1 + $summn1*3560;
+
+    else
+        $summn = $summn1 + ($summn1 +$summad)*3560;
+{
+    echo $summn;
+}
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <!DOCTYPE html>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/js/bootstrap.min.js"></script>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
     <link href="styles.css" rel="stylesheet">
     <link href="//db.onlinewebfonts.com/c/175e0e83cd889f912fed176709b63dfb?family=Europe" rel="stylesheet" type="text/css"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>jQuery UI Datepicker - Default functionality</title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+        $( function() {
+            $( "#datepicker1" ).datepicker();
+        } );
+    </script>
 </head>
 <body>
 <header>
@@ -34,13 +49,70 @@
             <li>Калькулятор</li>
         </ul>
     </nav>
+    <div class="container col-6">
+
+        <div class="row">
+
+
+        <div class="form-group col-6">
+            <label for="datepicker1">Калькулятор</label>
+            <p>Дата оформления вклада: <input type="text" id="datepicker1"></p>
+        </div>
+        </div>
+        <div class="form-group">
+            <label for="summvk">Сумма вклада</label>
+            <input type="text" class="form-control" id="summvk">
+        </div>
+        <div class="form-group">
+            <input type="range" class="form-control-range" min="1000" max="3000000" step="1" id="formControlRange" onchange="setValue1(this.value)">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Срок вклада</label>
+            <select class="form-control" id="srokvklada">
+                <option>1 год</option>
+                <option>2 года</option>
+                <option>3 года</option>
+                <option>4 года</option>
+                <option>5 лет</option>
+            </select>
+        </div>
+        <label for="exampleFormControlSelect1">Пополнение вклада</label>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="Popvk" id="Popvk1" value="false" checked>
+            <label class="form-check-label" for="Popvk1">
+                Нет
+            </label>
+        </div>
+
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="Popvk" id="Popvk2" value="true">
+            <label class="form-check-label" for="Popvk2">
+                Да
+            </label>
+        </div>
+        <div class="form-group">
+            <label for="summpvk">Сумма пополнения вклада</label>
+            <input type="text" class="form-control" id="summpvk">
+        </div>
+        <div class="form-group">
+            <input type="range" class="form-control-range" min="1000" max="3000000" step="1" id="formControlRange1" onchange="setValue(this.value)">
+        </div>
+        <button type="submit" class="btn btn-primary" onclick="Setdata()">Рассчитать</button>
+        <label for="exampleFormControlSelect1" id = "res">Результат: </label>
+    </div>
+
+    <!--<div>
+        <a href="about">Калькулятор</a>
+        <a href="about">Дата оформления вклада</a>
+        <a href="about">Сумма вклада</a>
+        <a href="about">Срок вклада</a>
+        <a href="about">Пополенение вклада</a>
+        <a href="about">Сумма пополнения вклада</a>
+    </div>
     <input type="submit" value="Рассчитать" onclick="Setdata()">
-    <p class="test">
+    -->
 
-    </p>
-    <article>
 
-    </article>
 </main>
 <footer>
     <div class="footer">
@@ -53,7 +125,7 @@
     </div>
 </footer>
 </body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 <script>
     function Setdata()
     {
@@ -61,11 +133,25 @@
         $.ajax({
             method: "POST",
             url: "calc.php",
-            data: { value: 1} })
+            data: {
+                datepicker1: $("#datepicker1").val(),
+                summvk: $("#summvk").val(),
+                srokvklada: $("#srokvklada").val(),
+                popvk: $('input[name=Popvk]:checked').val(),
+                summpvk: $("#summpvk").val(),
+            } })
             .done(function( response )
             {
-                $("p.test").html(response);
+                $("#res").html(response);
             });
+    }
+    function setValue(val)
+    {
+        document.getElementById('summpvk').value= val;
+    }
+    function setValue1(val)
+    {
+        document.getElementById('summvk').value= val;
     }
 </script>
 </html>
